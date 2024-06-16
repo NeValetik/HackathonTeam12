4a475ff444b191313aa0c8b281919b3aab065d91
janeta1 <janetagrigoras@gmail.com>
2024-06-16 05:40:36 +0300

    added css

--- 1/js/index.js
+++ 1/js/index.js
@@ -40,64 +40,78 @@ chrome.runtime.onMessage.addListener(async (request) => {
             // Use the response data to update the text body
             const text_body = document.querySelector(".text-body");
             text_body.innerHTML = `
-            <div>
-                <h3> The tech: ${responseData.current_product.name}</h3>
-                <h3> Price: ${responseData.current_product.details.price}</h3>
-                <ul>
-                <li> battery: ${responseData.current_product.details.scores.battery}</li>
-                <li> camera: ${responseData.current_product.details.scores.camera}</li>
-                <li> overall: ${responseData.current_product.details.scores.overall}</li>
-                <li> performance: ${responseData.current_product.details.scores.performance}</li>
-                <li> quality: ${responseData.current_product.details.scores.quality}</li>
-                <li> screen: ${responseData.current_product.details.scores.screen}</li>
-                <li> value: ${responseData.current_product.details.scores.value}</li>
-                </ul>
-            </div>
-            <div class = "row">
-            <h3> The tech: ${responseData.similar_products['0'].name}</h3>
-                <h3> Price: ${responseData.similar_products['0'].price}</h3>
-                <ul>
-                <li> battery: ${responseData.similar_products['0'].scores.battery}</li>
-                <li> camera: ${responseData.similar_products['0'].scores.camera}</li>
-                <li> overall: ${responseData.similar_products['0'].scores.overall}</li>
-                <li> performance: ${responseData.similar_products['0'].scores.performance}</li>
-                <li> quality: ${responseData.similar_products['0'].scores.quality}</li>
-                <li> screen: ${responseData.similar_products['0'].scores.screen}</li>
-                <li> value: ${responseData.similar_products['0'].scores.value}</li>
-                </ul>
-            <a href="${responseData.similar_products['0'].url}" target="_blank">Url 1</a>
-            </div>
-            <div class = "row">
-            <h3> The tech: ${responseData.similar_products['1'].name}</h3>
-                <h3> Price: ${responseData.similar_products['1'].price}</h3>
-                <ul>
-                <li> battery: ${responseData.similar_products['1'].scores.battery}</li>
-                <li> camera: ${responseData.similar_products['1'].scores.camera}</li>
-                <li> overall: ${responseData.similar_products['1'].scores.overall}</li>
-                <li> performance: ${responseData.similar_products['1'].scores.performance}</li>
-                <li> quality: ${responseData.similar_products['1'].scores.quality}</li>
-                <li> screen: ${responseData.similar_products['1'].scores.screen}</li>
-                <li> value: ${responseData.similar_products['1'].scores.value}</li>
-                </ul>
-            <a href="${responseData.similar_products['1'].url}" target="_blank">Url 2</a>
-
-            </div>
-            <div class = "col">
-            <h3> The tech: ${responseData.similar_products['2'].name}</h3>
-                <h3> Price: ${responseData.similar_products['2'].price}</h3>
-                <ul>
-                <li> battery: ${responseData.similar_products['2'].scores.battery}</li>
-                <li> camera: ${responseData.similar_products['2'].scores.camera}</li>
-                <li> overall: ${responseData.similar_products['2'].scores.overall}</li>
-                <li> performance: ${responseData.similar_products['2'].scores.performance}</li>
-                <li> quality: ${responseData.similar_products['2'].scores.quality}</li>
-                <li> screen: ${responseData.similar_products['2'].scores.screen}</li>
-                <li> value: ${responseData.similar_products['2'].scores.value}</li>
-                </ul>
-                <a href= "${responseData.similar_products['2'].url}" target="_blank">Url 3</a>
-
-            </div>         
-            `;
+            <table class="content-table">
+                <tr>
+                <th><h3>Item</h3></th>
+                <td><h3>${responseData.current_product.name}</h3></td>
+                <td><h3>${responseData.similar_products['0'].name}</h3></td>
+                <td><h3>${responseData.similar_products['1'].name}</h3></td>
+                <td><h3>${responseData.similar_products['2'].name}</h3></td>
+                </tr>
+                <th><h3>Price</h3></th>
+                <td>${responseData.current_product.details.price}</td>
+                <td>$${responseData.similar_products['0'].price}</td>
+                <td>$${responseData.similar_products['1'].price}</td>
+                <td>$${responseData.similar_products['2'].price}</td>
+                </tr>
+                <tr>
+                <th><h3>Battery</h3></th>
+                <td>${responseData.current_product.details.scores.battery}</td>
+                <td>${responseData.similar_products['0'].scores.battery}</td>
+                <td>${responseData.similar_products['1'].scores.battery}</td>
+                <td>${responseData.similar_products['2'].scores.battery}</td>
+                </tr>
+                <tr>
+                <th><h3>Camera</h3></th>
+                <td>${responseData.current_product.details.scores.camera}</td>
+                <td>${responseData.similar_products['0'].scores.camera}</td>
+                <td>${responseData.similar_products['1'].scores.camera}</td>
+                <td>${responseData.similar_products['2'].scores.camera}</td>
+                </tr>
+                <tr>
+                <th><h3>Overall</h3></th>
+                <td>${responseData.current_product.details.scores.overall}</td>
+                <td>${responseData.similar_products['0'].scores.overall}</td>
+                <td>${responseData.similar_products['1'].scores.overall}</td>
+                <td>${responseData.similar_products['2'].scores.overall}</td>
+                </tr>
+                <tr>
+                <th><h3>Performance</h3></th>
+                <td>${responseData.current_product.details.scores.performance}</td>
+                <td>${responseData.similar_products['0'].scores.performance}</td>
+                <td>${responseData.similar_products['1'].scores.performance}</td>
+                <td>${responseData.similar_products['2'].scores.performance}</td>
+                </tr>
+                <tr>
+                <th><h3>Quality</h3></th>
+                <td>${responseData.current_product.details.scores.quality}</td>
+                <td>${responseData.similar_products['0'].scores.quality}</td>
+                <td>${responseData.similar_products['1'].scores.quality}</td>
+                <td>${responseData.similar_products['2'].scores.quality}</td>
+                </tr>
+                <tr>
+                <th><h3>Screen</h3></th>
+                <td>${responseData.current_product.details.scores.screen}</td>
+                <td>${responseData.similar_products['0'].scores.screen}</td>
+                <td>${responseData.similar_products['1'].scores.screen}</td>
+                <td>${responseData.similar_products['2'].scores.screen}</td>
+                </tr>
+                <tr>
+                <th><h3>Value</h3></th>
+                <td>${responseData.current_product.details.scores.value}</td>
+                <td>${responseData.similar_products['0'].scores.value}</td>
+                <td>${responseData.similar_products['1'].scores.value}</td>
+                <td>${responseData.similar_products['2'].scores.value}</td>
+                </tr>
+                <tr>
+                <th><h3>Link</h3></th>
+                <td>-</td>
+                <td><a href="${responseData.similar_products['0'].url}" target="_blank">Link 1</a></td>
+                <td><a href="${responseData.similar_products['1'].url}" target="_blank">Link 2</a></td>
+                <td><a href="${responseData.similar_products['2'].url}" target="_blank">Link 3</a></td>
+                </tr>
+            </table>
+        `;
         } catch (error) {
             console.error("Error sending request to the server:", error);
         }
